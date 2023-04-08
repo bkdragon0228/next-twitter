acaaeb34ce783cd065298109b3aa3bd42a4fd572
bkdragon0228 <rlaqjarb2067@naver.com>
2023-04-08 14:01:36 +0900

    feat : Modal, Button 컴포넌트 생성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
+      {/* <Modal isOpen={false} title='test modal' actionLabel='Sumbit'/> */}
+      <Layout>
       <Component {...pageProps} />
-    </Layout>
+      </Layout>
+    </>
+
   )
 }
